fdecbad2acd7b93c56b3e509171b64c745b75852
dbrsva <okytti@mail.ru>
2025-12-17 10:03:50 +0900

    Add files via upload

---
+++ учебная практика.py
@@ -0,0 +1,57 @@
+#шахмат
+"""x = int(input("Ввдеите x: "))
+y = int(input("Введите y: "))
+
+if (x + 2) % 2 == 0: 
+    print("Черный")
+else: 
+    print("Белый")"""
+
+
+
+
+
+#2
+"""K = int(input("K: "))
+N = int(input("N: "))
+
+total = 0
+
+for i in range(K, N + 1):
+    if i % 2 == 0:
+        total += i
+
+print(total)"""
+
+#1
+"""x1 = int(input())
+y1 = int(input())
+x2 = int(input())
+y2 = int(input())
+if x1 > 8 or x1 < 1 or x2 > 8 or x2 < 1 or y1 > 8 or y1 < 1 or y2 > 8 or y2 < 1: 
+    print("Ошибка")
+else: 
+    if (abs(x1 - x2) == 2 and abs(y1 - y2) == 1) or (abs(x1 - x2) == 1 and abs(y1 - y2) == 2):
+        print("Да")
+    else:
+        print("Нет")"""
+ 
+#3       
+"""num = 0 
+
+while True: 
+    x = int(input("Введите число: "))
+    if x == 0: 
+        break 
+    num += x 
+print(num)"""
+
+#4
+"""N = int(input("Введите N:"))
+fact = 1 
+for i in range (1, N + 1): 
+    fact *= i 
+print(fact)"""
+
+
+
